54af57c6ec81f8236e3e0d95dab2c513b9347b21
domb <woon4910@gmail.com>
2023-03-02 15:54:26 +0900

    chore: Grouping

---
+++ ImageDownloader/Resources/URL.swift
@@ -0,0 +1,18 @@
+//
+//  URL.swift
+//  ImageDownloader
+//
+//  Created by 서동운 on 3/2/23.
+//
+
+import Foundation
+
+enum DummyURL {
+    static let data = ["https://picsum.photos/150/150",
+                       "https://picsum.photos/150/150",
+                       "https://picsum.photos/150/150",
+                       "https://picsum.photos/150/150",
+                       "https://picsum.photos/150/150",
+                       "https://picsum.photos/150/150",
+                       "https://picsum.photos/150/150"]
+}

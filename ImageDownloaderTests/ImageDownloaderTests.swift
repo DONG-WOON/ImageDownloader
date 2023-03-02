a23f24c90e3bf3a4d4754ed0d31b197b0e96055e
domb <woon4910@gmail.com>
2023-03-02 15:04:21 +0900

    feat: TDD ImageDownloader Test

--- ImageDownloaderTests/ImageDownloaderTests.swift
+++ ImageDownloaderTests/ImageDownloaderTests.swift
@@ -10,29 +10,37 @@ import XCTest
 
 final class ImageDownloaderTests: XCTestCase {
     
-    let sut = ImageDownloader()
+    let url = "https://picsum.photos/150/150"
+    
+    var imageDownloader: ImageDownloader!
+    var session: URLSession!
+    
+    func test_ImageDownloader_downloadImage() {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        let expectation = XCTestExpectation(description: "Download image")
+        
+        imageDownloader.downloadImage(stringURL: url) { result in
+            switch result {
+            case .success(let image):
+                XCTAssertNotNil(image)
+                expectation.fulfill()
+            case .failure(let error):
+                print(error)
+                XCTFail("Expected Test Success, but Load Image failure")
+            }
         }
+        wait(for: [expectation], timeout: 5.0)
     }
-
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        session = URLSession.shared
+        imageDownloader = ImageDownloader(session: session)
+    }
+    
+    override func tearDownWithError() throws {
+        session = nil
+        imageDownloader = nil
+        try super.tearDownWithError()
+    }
 }
